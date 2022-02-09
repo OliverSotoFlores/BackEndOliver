bd225c67ea221130bbd29df3d28f581b9284a34e
Oliver <oliversotof@gmail.com>
2022-02-09 16:34:58 -0600

    First commit

---
+++ src/main/java/com/backend/backendOliver/BackendOliverApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backendOliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/backendOliver/BackendOliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backendOliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
